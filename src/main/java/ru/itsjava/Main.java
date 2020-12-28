249b97c44ca471daad4599de2f337bd893fe984e
Valeriy <simha88@yandex.ru>
2020-12-28 19:27:27 +0000

    Добавлен урок 3

--- src/main/java/ru/itsjava/Main.java
+++ src/main/java/ru/itsjava/Main.java
@@ -1,32 +1,14 @@
 package ru.itsjava;
 
 import lombok.SneakyThrows;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+import ru.itsjava.services.ClientService;
+import ru.itsjava.services.ClientServiceImpl;
 
 public class Main {
 
-    public final static int PORT = 8001;
-    public final static String HOST = "localhost";
-
     @SneakyThrows
     public static void main(String[] args) {
-        Socket socket = new Socket(HOST, PORT);
-
-        if (socket.isConnected()) {
-            PrintWriter serverWriter = new PrintWriter(socket.getOutputStream());
-            serverWriter.println("Hi from client");
-            serverWriter.flush();
-
-            BufferedReader serverReader = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-
-            while (true) {
-                System.out.println(serverReader.readLine());
-            }
-        }
+        ClientService clientService=new ClientServiceImpl();
+        clientService.start();
     }
 }
