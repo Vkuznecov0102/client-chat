62eee465e0b0677236b122b15781718dbde0159a
Valeriy <simha88@yandex.ru>
2021-01-14 17:09:12 +0000

    Добавлены регистрация нового пользователя,авторизация,инструкция к чату.

--- src/main/java/ru/itsjava/Main.java
+++ src/main/java/ru/itsjava/Main.java
@@ -3,12 +3,13 @@ package ru.itsjava;
 import lombok.SneakyThrows;
 import ru.itsjava.services.ClientService;
 import ru.itsjava.services.ClientServiceImpl;
+import ru.itsjava.utils.Props;
 
 public class Main {
 
     @SneakyThrows
     public static void main(String[] args) {
-        ClientService clientService=new ClientServiceImpl();
+        ClientService clientService=new ClientServiceImpl(new Props());
         clientService.start();
     }
 }
--- src/main/java/ru/itsjava/services/ClientServiceImpl.java
+++ src/main/java/ru/itsjava/services/ClientServiceImpl.java
@@ -1,14 +1,26 @@
 package ru.itsjava.services;
 
 import lombok.SneakyThrows;
+import ru.itsjava.utils.Props;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.sql.*;
 
 public class ClientServiceImpl implements ClientService {
 
     public final static int PORT = 8001;
     public final static String HOST = "localhost";
+    private SocketRunnable socketRunnable;
+    private final Props props;
+    private BufferedReader bufferedReader;
+
+    public ClientServiceImpl(Props props) {
+        this.props = props;
+    }
+
 
     @SneakyThrows
     @Override
@@ -29,8 +41,15 @@ public class ClientServiceImpl implements ClientService {
                 System.out.println("Введите свой пароль");
                 String password = messageInputService.getMessage();
 
-                serverWriter.println("!autho!" + login + ":" + password);
-                serverWriter.flush();
+                if(userExists(login,password)) {
+                    printInstructions();
+                    serverWriter.println("!autho!" + login + ":" + password);
+                    serverWriter.flush();
+                }
+                else{
+                    System.out.println("Вы ввели неправильный логин или пароль.");
+                    printMenu(bufferedReader);
+                }
 
                 while (true) {
                     String consoleMessage = messageInputService.getMessage();
@@ -39,13 +58,22 @@ public class ClientServiceImpl implements ClientService {
                         System.exit(0);
                     }
                     else if(consoleMessage.startsWith("1")){
-                        System.out.println("Это тестовая единица");
+                        System.out.println("Введите будущий логин");
+                        String newName=messageInputService.getMessage();
+                        System.out.println("Введите будущий пароль");
+                        String newPassword=messageInputService.getMessage();
+                        if(createNewUser(newName,newPassword)){
+                            System.out.println("Новый пользователь успешно зарегистрирован. Нажмите 2 чтобы перезайти");
+                        }
+                        else{
+                            System.out.println("Что-то пошло не так");
+                        }
                     }
                     else if(consoleMessage.startsWith("2")){
-                        System.out.println("Это тестовая двойка");
+                        start();
                     }
                     else if(consoleMessage.startsWith("3")){
-                        System.out.println("Это тестовая три");
+                        System.exit(0);
                     }
                     serverWriter.println(consoleMessage);
                     serverWriter.flush();
@@ -55,6 +83,84 @@ public class ClientServiceImpl implements ClientService {
             }
         } catch (Exception exception) {
             exception.printStackTrace();
+        }
+
+    }
+
+    @SneakyThrows
+    private void printInstructions() {
+        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/instructions.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+    }
+
+    @SneakyThrows
+    private boolean createNewUser(String newName, String newPassword) {
+        try(
+
+                Connection connection = DriverManager.getConnection(
+                        props.getValue("db.url"),
+                        props.getValue("db.user"),
+                        props.getValue("db.password"))
+        ){
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("insert into chat_schema.users(name,password) values (?,?)");
+            preparedStatement.setString(1, newName);
+            preparedStatement.setString(2, newPassword);
+
+            preparedStatement.executeUpdate();
+            return true;
+        }
+        catch(SQLException sqlException){
+            sqlException.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @SneakyThrows
+    private boolean userExists(String login,String password) {
+        try(
+
+            Connection connection = DriverManager.getConnection(
+                    props.getValue("db.url"),
+                    props.getValue("db.user"),
+                    props.getValue("db.password"))
+        ){
+
+
+
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("select count(*) cnt from chat_schema.users where name=? and password=?;");
+            preparedStatement.setString(1, login);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+
+            int userCount = resultSet.getInt("cnt");
+
+            if (userCount == 1) {
+                return true;
+            }
+
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return false;
+    }
+
+    @SneakyThrows
+    private void printMenu(BufferedReader bufferedReader) {
+        while (true) {
+            System.out.println("Неправильные логин и пароль. Вы хотите зарегистрироваться?\n" +
+                    "1)Да\n" +
+                    "2)Ввести данные заново\n" +
+                    "3)Выйти из приложения\n");
+            if(bufferedReader == null ) break;
         }
     }
 }
